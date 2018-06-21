b40339109c77db25904acde3e30722833473972b
tomerberman <tomerberman@gmail.com>
2018-06-21 12:01:04 +0300

    fixed canvas stretching, added text render function

--- js/main.js
+++ js/main.js
@@ -1,50 +1,46 @@
-'use strict'
-
+"use strict";
 
 function togglePages(photoId) {
   var elMain = document.querySelector("main");
   var elEditor = document.querySelector("editor");
   elMain.classList.toggle("hidden");
   elEditor.classList.toggle("hidden");
-  if (photoId!==undefined) renderEditingCanvas(photoId);
+  if (photoId !== undefined) renderEditingCanvas(photoId);
 }
-
-
 
 function renderGallery() {
   var elGallery = document.querySelector(".meme-container");
   elGallery.innerHTML = setImagesForRendering();
 }
 
-//need to add the text rendering seperatly.
+function renderText() {
+  var elCanvas = document.querySelector("#canvas");
+  var ctx = elCanvas.getContext("2d");
 
-function renderText(){}
+  var currMeme = getCurrMeme();
+  for (let i = 0; i < currMeme.txts.length; i++) {
+    var txt = currMeme.txts[i];
+    ctx.font = txt.fontSize + 'px ' + txt.fontFamily;
+    ctx.fillStyle = txt.color;
+    ctx.textAlign = txt.alignHor;
+    ctx.fillText(txt.line, canvas.width / 2, canvas.height * txt.posVert);
+  }
+}
 
 function renderEditingCanvas(id) {
   var img = new Image();
   img.src = getImageById(id).url;
-  console.log("img.src=", img.src);
   var elCanvas = document.querySelector("#canvas");
   var elCanvasContainer = document.querySelector(".canvas-container");
   var ctx = elCanvas.getContext("2d");
-  img.onload = function () {
-    if (img.width > window.innerWidth ) {
-      var oversized = img.width / (window.innerWidth );
-      img.height = img.height / oversized;
-      img.width = img.width / oversized;
-    }
 
-    elCanvas.height = img.height;
-    elCanvas.width = img.width;
-    ctx.drawImage(img, 0, 0);
-    ctx.font = '48px impact';
-    ctx.fillStyle = '#7f0f0f';
-    ctx.textAlign = 'center';
-    ctx.fillText('Hello world...', canvas.width / 2, canvas.height * 0.2);
-    ctx.fillText('I\'ll Be Back...', canvas.width / 2, canvas.height * 0.85);
+  img.onload = function() {
+    var aspect = img.width / img.height;
+    elCanvas.width = elCanvasContainer.offsetWidth - 4;
+    elCanvas.height = elCanvas.width / aspect;
+    elCanvasContainer.height = elCanvas.height + 4;
+    ctx.drawImage(img, 0, 0, elCanvas.width, elCanvas.height);
 
-    ctx.fillStyle = 'yellow';
-    ctx.fillText('Hello world...', canvas.width / 2 - 3, canvas.height * 0.2 - 4);
-    ctx.fillText('I\'ll Be Back...', canvas.width / 2 - 3, canvas.height * 0.85 - 4);
-  }
+    renderText();
+  };
 }
--- js/meme-services.js
+++ js/meme-services.js
@@ -31,7 +31,8 @@ var gImgs = [
 var gMeme = {
     selectedImgId: 5,
     txts: [
-        { line: 'I never eat Falafel', size: 20, align: 'left', color: 'red' }
+        { line: 'I never eat Falafel', fontFamily: 'impact', fontSize: 40, alignHor: 'center', posVert: 0.13, color: 'red' },
+        { line: 'But Im Hungry', fontFamily: 'arial', fontSize: 50, alignHor: 'center', posVert: 0.9, color: 'yellow' }
     ]
 }
 
@@ -55,3 +56,6 @@ function getImageById(id){
     return null;
 }
 
+function getCurrMeme(){
+    return gMeme;
+}
